5d41bb6546e1bc5837834146af2654913f321189
denvilk <vvelikovich@gmail.com>
2020-09-14 23:32:35 +0300

    Updates

---
+++ precache-manifest.e474051cd06d6e80370ed402ff274241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad61c6707217d93b33ff441c098b5ac4",
+    "url": "/vk-mini-apps/index.html"
+  },
+  {
+    "revision": "4ac2419d1544978daaee",
+    "url": "/vk-mini-apps/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "4ac2419d1544978daaee",
+    "url": "/vk-mini-apps/static/js/2.1aa946a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-mini-apps/static/js/2.1aa946a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cfc02c6ecf301a45c72",
+    "url": "/vk-mini-apps/static/js/main.1c4a6648.chunk.js"
+  },
+  {
+    "revision": "6014772779d31b8f85e2",
+    "url": "/vk-mini-apps/static/js/runtime-main.d471473d.js"
+  }
+]);
